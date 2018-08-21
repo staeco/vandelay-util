a3f5d63af67b2771522029d00a07803d365d26fb
contra <yo@contra.io>
2018-08-21 19:04:44 -0400

    f2c integrity fixes

--- dist/formatting/f2c.js
+++ dist/formatting/f2c.js
@@ -9,7 +9,10 @@ var _fahrenheitToCelsius2 = _interopRequireDefault(_fahrenheitToCelsius);
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 exports.default = v => {
+  if (v == null) return v;
   if (typeof v === 'string') v = parseFloat(v);
+  const t = typeof v;
+  if (t !== 'number') throw new Error(`Expected number, got: ${t}`);
   return (0, _fahrenheitToCelsius2.default)(v);
 };
 
--- src/formatting/f2c.js
+++ src/formatting/f2c.js
@@ -1,6 +1,9 @@
 import f2c from 'fahrenheit-to-celsius'
 
 export default (v) => {
+  if (v == null) return v
   if (typeof v === 'string') v = parseFloat(v)
+  const t = typeof v
+  if (t !== 'number') throw new Error(`Expected number, got: ${t}`)
   return f2c(v)
 }
