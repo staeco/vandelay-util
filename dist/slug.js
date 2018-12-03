8ce2815ebceea65163f95b83bb68de64492139ae
contra <yo@contra.io>
2018-12-03 17:19:46 -0500

    slug dates right

--- dist/slug.js
+++ dist/slug.js
@@ -9,7 +9,7 @@ var _slugify2 = _interopRequireDefault(_slugify);
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 exports.default = (...v) => {
-  const args = v.filter(i => i != null);
+  const args = v.filter(i => i != null).map(v => v instanceof Date ? v.toLocaleDateString() : v);
   if (args.length === 0) return;
   return (0, _slugify2.default)(args.join(' ')).toLowerCase();
 };
--- src/slug.js
+++ src/slug.js
@@ -2,7 +2,9 @@ import slugify from '@sindresorhus/slugify'
 
 
 export default (...v) => {
-  const args = v.filter((i) => i != null)
+  const args = v
+    .filter((i) => i != null)
+    .map((v) => v instanceof Date ? v.toLocaleDateString() : v)
   if (args.length === 0) return
   return slugify(args.join(' ')).toLowerCase()
 }
