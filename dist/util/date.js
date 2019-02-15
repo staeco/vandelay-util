06acec18ba53e5cf39a9c22118e2bb4776084b07
Sam McCollum <smccollum@mccollum.enterprises>
2019-02-14 19:18:41 -0700

    add additional tests and add comments to code

--- dist/util/date.js
+++ dist/util/date.js
@@ -23,12 +23,20 @@ const produceDateFormat = timestamp => {
 exports.default = (v, tz) => {
   if (!tz) throw new Error('Missing timezone (in util.date)');
   if (v == null) return;
-  const dv = new Date(v);
+  const dv = new Date(v); // js Date does a good job of parsing/coercing weird strings to a date, do this now and hang onto it for later
   if (isNaN(dv)) return;
-  const epoch = parseNum(v);
+  const epoch = parseNum(v); // try to parse the input number or string to a number which will be interpreted as epoch time
   if (epoch) {
+    // if it's epoch time, we can use unix in moment
     return produceDateFormat(_momentTimezone2.default.unix(epoch / 1000).utc());
   }
+  if (/^\d{4}-\d\d-\d\dT\d\d:\d\d:\d\d(\.\d+)?(([+-]\d\d:\d\d)|Z)?$/i.test(v)) {
+    //check if it matches ISO 8601 with a regex; necessary because moment will sometimes parse things that aren't true ISO 8601 even when set to strict
+    const mt = (0, _momentTimezone2.default)(v, _momentTimezone2.default.ISO_8601, true); //true to use strict parsing in moment
+    if (mt.isValid()) return produceDateFormat(mt);
+  }
+
+  // if we've gotten this far, assume that js Date got the time wrong due to timezone issues and yank the local values out so we can build a string for moment to parse, eliminating the need to correct for weird timezone stuff
   const dateString = `${dv.getFullYear()}-${pad(dv.getMonth() + 1)}-${pad(dv.getDate())} ${pad(dv.getHours())}:${pad(dv.getMinutes())}:${pad(dv.getSeconds())}`;
   return produceDateFormat(_momentTimezone2.default.tz(dateString, 'YYYY-MM-DD HH:mm:ss', tz).utc());
 };
--- test/date.js
+++ test/date.js
@@ -25,8 +25,22 @@ describe('date', () => {
     util.date('12/27/1993', 'America/New_York').should.equal('1993-12-27T05:00:00.000Z')
     util.date('12/27/93 00:00', 'America/New_York').should.equal('1993-12-27T05:00:00.000Z')
   })
+  it('should handle ISO string inputs', async () => {
+    util.date('1993-12-27T08:00:00.000Z', 'America/New_York').should.equal('1993-12-27T08:00:00.000Z')
+    util.date('1993-12-27T05:00:00.000Z', 'America/Los_Angeles').should.equal('1993-12-27T05:00:00.000Z')
+  })
   it('should return a valid date from number', async () => {
+    // EST
     util.date(1000, 'America/New_York').should.equal('1970-01-01T00:00:01.000Z')
+    util.date('1000', 'America/New_York').should.equal('1970-01-01T00:00:01.000Z')
+
+    // PST
+    util.date(1000, 'America/Los_Angeles').should.equal('1970-01-01T00:00:01.000Z')
+    util.date('1000', 'America/Los_Angeles').should.equal('1970-01-01T00:00:01.000Z')
+  })
+  it('should return valid date from preparsed date', async () => {
+    util.date(new Date('12/27/1993'), 'America/Los_Angeles').should.equal('1993-12-27T08:00:00.000Z')
+    util.date(new Date('12/27/1993'), 'America/New_York').should.equal('1993-12-27T05:00:00.000Z')
   })
   it('should return a valid date from number with timezone', async () => {
     util.date('2014-06-01 12:00', 'America/New_York').should.equal('2014-06-01T16:00:00.000Z')
