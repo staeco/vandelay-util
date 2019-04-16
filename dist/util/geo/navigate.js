fc94cebe8904fadf7ace784d6e5b8d438c019c1e
contra <yo@contra.io>
2019-04-16 10:17:20 -0400

    3.7.1

--- dist/util/geo/navigate.js
+++ dist/util/geo/navigate.js
@@ -16,6 +16,10 @@ var _quickLru2 = _interopRequireDefault(_quickLru);
 
 var _http = require('http');
 
+var _geojsonPrecision = require('geojson-precision');
+
+var _geojsonPrecision2 = _interopRequireDefault(_geojsonPrecision);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 const { pelias } = global.__vandelay_util_config;
@@ -38,14 +42,17 @@ exports.default = async ({ type, start, end, optional }) => {
   if (start.type !== 'Point') throw new Error('Invalid start type, expected Point (in geo.navigate)');
   if (end.type !== 'Point') throw new Error('Invalid end type, expected Point (in geo.navigate)');
 
+  const startPoint = (0, _geojsonPrecision2.default)(start, 6); // set fixed precision as Valhalla can be picky
+  const endPoint = (0, _geojsonPrecision2.default)(end, 6);
+
   const path = {
     type: 'LineString',
-    coordinates: [start.coordinates, end.coordinates]
+    coordinates: [startPoint.coordinates, endPoint.coordinates]
   };
   const q = {
     json: JSON.stringify({
       costing: types[type],
-      locations: [{ lat: start.coordinates[1], lon: start.coordinates[0] }, { lat: end.coordinates[1], lon: end.coordinates[0] }]
+      locations: [{ lat: startPoint.coordinates[1], lon: startPoint.coordinates[0] }, { lat: endPoint.coordinates[1], lon: endPoint.coordinates[0] }]
     })
     // check if cache has it first
   };const lruKey = JSON.stringify(q);
@@ -55,7 +62,7 @@ exports.default = async ({ type, start, end, optional }) => {
   let out;
   try {
     const { body } = await _superagent2.default.get(pelias.hosts.route).retry(10).set('apikey', pelias.key).type('json').agent(agent).query(q);
-    out = _polyline2.default.toGeoJSON(body.trip.legs[0].shape);
+    out = _polyline2.default.toGeoJSON(body.trip.legs[0].shape, 6); // decode using Valhalla's fixed precision
   } catch (err) {
     if (!optional) {
       if (err.response && err.response.body && err.response.body.error) {
